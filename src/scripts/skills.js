50e2037336f2dcaca5e9c4f4b1f66770bf3e6e02
Nikita Makarin <n-makarin@mail.ru>
2020-01-11 17:09:12 +0300

    Add script to src\scripts\skills.js

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,48 @@
-console.log('this is skills module');
+window.onload = () => {
+  fillCharts()
+}
+
+const selector = {
+  chart: '.skills-item__chart',
+  round: '.skills-item__round'
+}
+const attr = {
+  radius: 'r',
+  percent: 'percent'
+}
+const strokeParams = {
+  a: 0,
+  b: '999'
+}
+
+/**
+ * Fill charts 'stroke-asharray' attribute by it's percentage
+ * @returns void
+ */
+function fillCharts() {
+  const chartList = document.querySelectorAll(selector.chart)
+  chartList.forEach((element) => {
+    calculateStrokePercent(element)
+    setStrokeDasharray(element)
+  })
+}
+
+/**
+ * @param {object} svgContainer 
+ * @returns void
+ */
+function calculateStrokePercent(svgContainer) {
+  const round = svgContainer.querySelector(selector.round)
+  const radius = round.getAttribute(attr.radius)
+  const percent = svgContainer.getAttribute(attr.percent)
+  const circum = 2 * radius * Math.PI
+  strokeParams.a = percent * circum / 100;
+}
+/**
+ * @param {object} svgContainer
+ * @returns void
+ */
+function setStrokeDasharray(svgContainer) {
+  const round = svgContainer.querySelector(selector.round)
+  round.style.strokeDasharray = `${strokeParams.a} ${strokeParams.b}`
+}
