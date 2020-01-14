d30587e0869a6cad2e5c908beaff05ddc804f8a6
Nikita Makarin <n-makarin@mail.ru>
2020-01-14 20:50:53 +0300

    Add methods to src\scripts\projects.js

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,55 +1,170 @@
 import Vue from 'vue'
 import db from '../../db-temp.json'
 
+const start = 0
+const end = 1
+
 const App = new Vue({
   el: '#projects-app',
   data() {
     return {
       projectList: db.projectList,
       project: db.projectList[0],
-      slider: {
-        trackMaxLength: 4
+      track: {
+        itemClass: 'projects-slider__item',
+        itemClassActive: 'projects-slider__item_active',
+        maxLength: 4,
+        projectList: []
       },
-      trackItemClass: 'projects-slider__item',
-      trackItemClassAcrive: 'projects-slider__item_active',
+      trackActivePosition: 0,
+      trackRange: [0, 0]
+    }
+  },
+  mounted() {
+    this.setTrackRange([0, this.track.maxLength])
+    this.setTrackProjectList()
+  },
+  watch: {
+    trackActivePosition(newValue) {
+      this.project = this.projectList[newValue]
     }
   },
   methods: {
-    setProject(itemId) {
-      this.projectList.forEach(element => {
-        if (element.id !== itemId) { return }
-        this.project = element
-      });
+    /**
+     * @param {array} range 
+     * @returns void
+     */
+    setTrackRange(range) {
+      this.trackRange = range
     },
     /**
-     * Change project in slider to next item
+     * @returns void
+     */
+    setTrackProjectList() {
+      this.track.projectList = this.projectList.slice(
+        this.trackRange[start], this.trackRange[end]
+      )
+    },
+    /**
+     * @param {number} itemId
+     * @returns void
+     */
+    setProject(itemId) {
+      for (let i = 0; i < this.projectList.length; i++) {
+        if (this.projectList[i].id !== itemId) { continue }
+        this.trackActivePosition = i
+        break
+      }
+    },
+    /**
      * @returns void
      */
     next() {
-      let index = 0
-      for (let i = 0; i < this.projectList.length; i++) {
-        if (this.projectList[i].id !== this.project.id) { continue }
-        if (i < this.projectList.length - 1) {
-          index = i + 1
-        }
-        this.project = this.projectList[index]
-        break
+      this.shiftActivePositionForvard()
+      if (this.needShiftRangeForvard()) {
+        this.shiftTrackRangeForvard()
       }
+      if (this.needShiftRangeToStart()) { 
+        this.shiftTrackRangeToStart()
+      }
+      this.setTrackProjectList()
     },
     /**
-     * Change project in slider to prev item
      * @returns void
      */
     prev() {
-      let index = this.projectList.length - 1
-      for (let i = 0; i < this.projectList.length; i++) {
-        if (this.projectList[i].id !== this.project.id) { continue }
-        if (i !== 0) {
-          index = i - 1
-        }
-        this.project = this.projectList[index]
-        break
+      this.shiftActivePositionBack()
+      if (this.needShiftRangeBack()) {
+        this.shiftTrackRangeBack()
       }
+      if (this.needShiftRangeToEnd()) {
+        this.shiftTrackRangeToEnd()
+      }
+      this.setTrackProjectList()
+    },
+    /**
+     * @returns void
+     */
+    shiftActivePositionForvard() {
+      if (this.needSetActivePositionToStart()) {
+        this.trackActivePosition = 0
+      } else {
+        this.trackActivePosition++
+      }
+    },
+    /**
+     * @returns void
+     */
+    shiftActivePositionBack() {
+      if (this.needSetActivePositionToEnd()) {
+        this.trackActivePosition = this.projectList.length - 1
+      } else {
+        this.trackActivePosition--
+      }
+    },
+    /**
+     * @returns void
+     */
+    shiftTrackRangeForvard() {
+      this.trackRange[start]++
+      this.trackRange[end]++
+    },
+    /**
+     * @returns void
+     */
+    shiftTrackRangeToStart() {
+      this.trackRange[start] = this.trackActivePosition
+      this.trackRange[end] = this.trackActivePosition + this.track.maxLength
+    },
+    /**
+     * @returns void
+     */
+    shiftTrackRangeBack() {
+      this.trackRange[start]--
+      this.trackRange[end]--
+    },
+    /**
+     * @returns void
+     */
+    shiftTrackRangeToEnd() {
+      this.trackRange[start] = this.trackActivePosition + 1 - this.track.maxLength
+      this.trackRange[end] = this.trackActivePosition + 1
+    },
+    /**
+     * @returns {boolean}
+     */
+    needShiftRangeForvard() {
+      return this.trackActivePosition + 1 > this.trackRange[end]
+    },
+    /**
+     * @returns {boolean}
+     */
+    needShiftRangeBack() {
+      return this.trackActivePosition < this.trackRange[start]
+    },
+    /**
+     * @returns {boolean}
+     */
+    needShiftRangeToEnd() {
+      return this.trackActivePosition + 1 > this.trackRange[end]
+    },
+    /**
+     * @returns {boolean}
+     */
+    needShiftRangeToStart() {
+      return this.trackActivePosition < this.trackRange[start]
+    },
+    /**
+     * @returns {boolean}
+     */
+    needSetActivePositionToStart() {
+      return this.trackActivePosition + 1 > this.projectList.length - 1
+    },
+    /**
+     * @returns {boolean}
+     */
+    needSetActivePositionToEnd() {
+      return this.trackActivePosition - 1 < 0
     }
   }
 })
